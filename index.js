04ffc5c134b444015bba2b5e3d99a69c54f4098b
maccyber <jonas.enge@gmail.com>
2016-08-05 10:38:24 +0200

    Fixed paths

--- index.js
+++ index.js
@@ -11,13 +11,14 @@ module.exports = function transportByNsb (options) {
     throw new Error('Missing required input: options.skoleid')
   }
 
+  const basePath = require('path').resolve(__dirname)
   const isFile = require('is-file')
   const skoleid = parseInt(options.skoleid, 10)
-  const fileName = 'lib/data/' + skoleid + '.json'
+  const fileName = basePath + '/lib/data/' + skoleid + '.json'
   const postnummer = parseInt(options.postnummer, 10)
 
   if (isFile(fileName)) {
-    const zips = require('./' + fileName)
+    const zips = require(fileName)
     return zips.indexOf(postnummer) > -1
   } else {
     return false
